5d952ba15e5abf2f589fe1c0eff7a93e9649e2cb
ColdStep <fantomik4407@gmail.com>
2018-02-25 12:20:38 +0200

    1.0

---
+++ src/main/java/servlets/BLibrary.java
@@ -0,0 +1,7 @@
+package servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/bLibrary")
+public class BLibrary extends HttpServlet {
+}
---
+++ src/main/java/servlets/MLibrary.java
@@ -0,0 +1,7 @@
+package servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/mLibrary")
+public class MLibrary extends HttpServlet {
+}
