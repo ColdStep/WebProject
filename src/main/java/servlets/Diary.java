95a303247cb9cd4187f2dce5e5580e44d8e8a72e
ColdStep <fantomik4407@gmail.com>
2018-02-25 14:39:37 +0200

    1.1

--- src/main/java/servlets/Diary.java
+++ src/main/java/servlets/Diary.java
@@ -1,16 +1,23 @@
 package servlets;
 
+import org.apache.commons.io.FileUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet("/diary")
 public class Diary extends HttpServlet{
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        File file = new File("C:\\Users\\vbaku\\IdeaProjects\\webproject\\src\\main\\webapp\\Files\\diary.txt");
+        String textDiary = req.getParameter("reqDiary");
+        FileUtils.write(file,textDiary);
+        resp.getWriter().write("Save");
     }
 }
